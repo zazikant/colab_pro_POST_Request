6918f16557594ecce8a7e2069cd2c0da37f2c842
Shashikant Zarekar <zazikant@gmail.com>
2023-10-20 09:47:38 +0530

    s

--- app.py
+++ app.py
@@ -4,14 +4,19 @@ from slack_sdk.errors import SlackApiError
 from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_bolt import App
 from dotenv import find_dotenv, load_dotenv
+from functions import draft_email
 import requests
 import json
 import time
 
 from flask_ngrok import run_with_ngrok
-from flask import Flask, request
+from flask import Flask, render_template, request
 
-from functions import draft_email
+import torch
+from diffusers import StableDiffusionPipeline
+
+import base64
+from io import BytesIO
 
 # Load environment variables from .env file
 load_dotenv(find_dotenv())
@@ -25,15 +30,27 @@ SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
 app = App(token=SLACK_BOT_TOKEN)
 
 # Initialize the Flask app
+# Flask is a web application framework written in Python
 flask_app = Flask(__name__)
 run_with_ngrok(flask_app)
 handler = SlackRequestHandler(app)
 
-# Initialize variables to store the result of draft_email
-cached_response = None
-cached_text = None
+# # Define a function to keep Colab active
+# def keep_colab_active():
+#     while True:
+#         print("Colab is still active")
+#         time.sleep(300)  # Sleep for 5 minutes
+
+# Start the function in a separate thread to keep running in the background
+# import threading
+# threading.Thread(target=keep_colab_active).start()
 
 def get_bot_user_id():
+    """
+    Get the bot user ID using the Slack API.
+    Returns:
+        str: The bot user ID.
+    """
     try:
         # Initialize the Slack client with your bot token
         slack_client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
@@ -42,40 +59,73 @@ def get_bot_user_id():
     except SlackApiError as e:
         print(f"Error: {e}")
 
-@flask_app.route("/get_response", methods=["GET"])
-def get_response():
-    global cached_response, cached_text
 
-    text = request.args.get("text", "")
+def my_function(text):
+    """
+    Custom function to process the text and return a response.
+    In this example, the function converts the input text to uppercase.
 
-    # Check if the text has changed
-    if text != cached_text:
-        # If the text has changed, call draft_email and update the cache
-        email, response = draft_email(text)
-        cached_text = text
-        cached_response = {"email": email, "response": response}
+    Args:
+        text (str): The input text to process.
 
-    return cached_response
+    Returns:
+        str: The processed text.
+    """
+    response = text.upper()
+    return response
+
 
 @app.event("app_mention")
 def handle_mentions(body, say):
-    global cached_response, cached_text
+    """
+    Event listener for mentions in Slack.
+    When the bot is mentioned, this function processes the text and sends a response.
 
+    Args:
+        body (dict): The event data received from Slack.
+        say (callable): A function for sending a response to the channel.
+    """
     text = body["event"]["text"]
+
     mention = f"<@{SLACK_BOT_USER_ID}>"
     text = text.replace(mention, "").strip()
+
     say("Sure, I'll get right on that!")
+    # response = my_function(text)
+    
+    # Extract the email from the text
+    email, response = draft_email(text)
+    
+    # Make the POST request
+    url = "https://hook.us1.make.com/ohyonocw701n4ynie637qcm3roe3yrhn"
+    headers = {"Content-Type": "application/json"}
+    payload = {"email": email, "response": response}
+    data = json.dumps(payload)
+    # data = {"response": response}
+    
+    # post_response = requests.post(url, headers=headers, json=data)
+    
+    post_response = requests.post(url, headers=headers, data=data)
 
-    # Update the cache with the latest text
-    cached_text = text
-    # Reset the cached response to None, so that it gets recalculated in the next GET request
-    cached_response = None
+    # Check the response status code
+    if post_response.status_code == 200:
+        say("POST request successful")
+    else:
+        say("POST request failed")
 
-    # No need to return anything here, as this is an event handler
 
 @flask_app.route("/slack/events", methods=["POST"])
 def slack_events():
+    """
+    Route for handling Slack events.
+    This function passes the incoming HTTP request to the SlackRequestHandler for processing.
+
+    Returns:
+        Response: The result of handling the request.
+    """
     return handler.handle(request)
 
+
+# Run the Flask app
 if __name__ == "__main__":
     flask_app.run()
